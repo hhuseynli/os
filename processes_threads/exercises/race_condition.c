02361e3282a954eff69ba9b77b30dc3065747c2f
Huseyn Huseynli <ht.huseynli@gmail.com>
2026-03-09 14:33:02 +0400

    Race Conditions

---
+++ processes_threads/exercises/race_condition.c
@@ -0,0 +1,41 @@
+#include <pthread.h> // POSIX Threads
+#include <stdio.h>   // Standard I/O - print
+#include <stdlib.h>  // Standard lib - memory
+#include <unistd.h>  // UNIX Standard - sleep
+
+#define NUMBER_OF_THREADS 10 
+
+int var = 0;
+
+void *increment_var(void *tid){
+    // This function increments var and 'blames' a thread for a certain value
+    printf("Thread %p made var = %d\n", tid, ++var);
+    pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[]){
+
+    // The main program creates n incrementer threads and then exits.
+
+    pthread_t threads[NUMBER_OF_THREADS];
+    int status, i;
+
+   for(i=0; i < NUMBER_OF_THREADS; i++) {
+
+        printf("Main here. Creating thread %d\n", i);
+        status = pthread_create(&threads[i], NULL, increment_var, (void *)i);
+
+        if (status != 0) {
+            printf("Oops. pthread create returned error code %d\n", status);
+            exit(-1);
+        }
+  	/* 
+   	 if (status = pthread_join(threads[i], NULL) != 0){
+      		 printf("Oops. pthread join returned error code %d\n", status);
+       		exit(-1);
+   	 }
+	 */
+}
+    exit(0);
+
+}
